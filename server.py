286f853d1619bbaa5d9570ba76b8d2dcb878a982
the-abra <egekilic@proton.me>
2024-12-22 23:55:53 +0300

    aa

--- server.py
+++ server.py
@@ -7,8 +7,8 @@ def index():
     return render_template('index.html')
 
 @app.route('/load')
-def index():
+def load():
     return render_template('load.html')
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    app.run(port=3000, debug=True, host="127.0.0.1")
