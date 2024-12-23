bb2b57c953feb6ad88a751f6fd1515f1ee1d5a33
the-abra <egekilic@proton.me>
2024-12-23 15:15:04 +0300

    aa

--- server.py
+++ server.py
@@ -2,17 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/<string:page_name>')
+@app.route('/<page_name>')
 def root(page_name):
-    if page_name != "nothing":
-        return render_template('load.html', fetchpage=page_name)
+    if page_name == "nothing":
+       return render_template('load.html', fetchpage="index")
+       
     else:
         try:
             return render_template(f'{page_name}.html')
         except Exception:
             return render_template('404.html'), 404
+        
 
-@app.route('/fetch/<string:page_name>')
+@app.route('/fetch/<page_name>')
 def fetch(page_name):
     # Dynamically load the requested template
     return render_template(f'{page_name}.html')
