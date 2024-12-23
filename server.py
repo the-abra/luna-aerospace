4ad9894667402f5c423d05e0d86cc6b6b4c0b52c
the-abra <egekilic@proton.me>
2024-12-23 16:24:18 +0300

    aaaaaa

--- server.py
+++ server.py
@@ -1,18 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import os
+
 app = Flask(__name__)
+FONT_DIR = os.path.join(app.root_path, 'static', 'fonts')
 
 @app.route('/')
 def root():
     try:
-        return render_template("index.html")
+        return render_template("load.html", fetchpage="index"), 200
     except Exception:
         return render_template('404.html'), 404
 
 @app.route('/<string:page_name>')
 def rootplus(page_name):
     if os.path.isfile(f"./templates/{page_name}.html"):
-        return render_template("load.html", pagefetch=page_name)
+        return render_template("load.html", fetchpage=page_name), 200
     else:
         return render_template('404.html'), 404
         
@@ -20,7 +22,14 @@ def rootplus(page_name):
 @app.route('/fetch/<string:page_name>')
 def fetch(page_name):
     # Dynamically load the requested template
-    return render_template(f'{page_name}.html')
+    if os.path.isfile(f"./templates/{page_name}.html"):
+        return render_template(f'{page_name}.html'), 200
+    else:
+        return render_template('404.html'), 404
+
+@app.route('/fonts/<path:filename>')
+def serve_font(filename):
+    return send_from_directory(FONT_DIR, filename, mimetype='font/woff')
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
--- static/load.js
+++ static/load.js
@@ -1,11 +1,11 @@
 const iframe = document.getElementById('iframe');
   
   // Event listener to check when the iframe content is loaded
-iframe.onload = function() {
+  iframe.onload = function() {
     // Hide the loading animation once iframe content is loaded
-    document.querySelector('.thing').style.display = 'none';
+    document.getElementById('thing').style.display = 'none';
     
     // Show the content and the iframe
-    document.querySelector('.content').style.display = 'block';
+    document.getElementById('content').style.display = 'block';
     iframe.style.display = 'block';
   };
