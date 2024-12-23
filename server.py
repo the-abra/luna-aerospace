1f679049acc147b50d2f8a11dfc1d2a42ef45c90
the-abra <egekilic@proton.me>
2024-12-23 15:00:50 +0300

    aa

--- server.py
+++ server.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/<string:page_name>')
+def root(page_name):
+    if page_name == '':
+        return render_template('load.html', fetchpage="index")
+    else:
+        return render_template('load.html', fetchpage=page_name)
 
-@app.route('/load')
-def index():
-    return render_template('load.html')
+@app.route('/fetch/<string:page_name>')
+def fetch(page_name):
+    # Dynamically load the requested template
+    return render_template(f'{page_name}.html')
 
 if __name__ == '__main__':
-    app.run(port=80, host='0.0.0.0')
+    app.run(port=3000, host='0.0.0.0')
