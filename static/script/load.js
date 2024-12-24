4842f831fbda3f29c40b781b9ba35e28c8de3d5e
the-abra <egekilic@proton.me>
2024-12-24 16:53:13 +0300

    aa

--- static/script/load.js
+++ static/script/load.js
@@ -1,7 +1,6 @@
 const iframe = document.getElementById('iframe');
-  
-  // Event listener to check when the iframe content is loaded
-  iframe.onload = function() {
+
+iframe.onload = function() {
     // Hide the loading animation once iframe content is loaded
     document.getElementById('thing').style.display = 'none';
     
